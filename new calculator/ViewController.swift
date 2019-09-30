08fe94f16d6137a37ab313fdceb33518e981274c
saad <saadbaig200@gmail.com>
2019-09-30 12:17:24 +0500

    made a new calculator

--- new calculator/ViewController.swift
+++ new calculator/ViewController.swift
@@ -10,11 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flip=0
+    var c=""
+    var num1:String="0"
+    var num2:String="0"
+    var operation="+"
+    @IBOutlet weak var label1: UILabel!
+    
+    
+    @IBAction func backspace(_ sender: UIButton) {
+        
+        c=String(c.dropLast())
+        label1.text=c
     }
-
+    @IBAction func numbers_pressed(_ sender: UIButton) {
+        num1="0"
+        num2="0"
+        let a:String=sender.currentTitle!
+        c=c+a
+        label1.text=c
+        flip=0
+    }
+    
+    @IBAction func evaluate(_ sender: UIButton) {
+        
+        for i in c{
+            
+            let d=String(i)
+            if i == "+" || i == "*" || i == "-" || i == "÷"{
+                operation=String(i)
+                flip=1
+                continue
+            }
+            if flip==0{
+                num1+=d
+            }
+            else{
+                num2+=d
+            }
+            
+        }
+        if operation=="+"{
+            label1.text =  String(Float(num1)!+Float(num2)!)
+            num1=String(Float(num1)!+Float(num2)!)
+        }
+        else if operation=="-"{
+            label1.text = String(Int(num1)!-Int(num2)!)
+            num1=String(Float(num1)!-Float(num2)!)
+        }
+        else if operation=="*"{
+            label1.text = String(Float(num1)!*Float(num2)!)
+            num1=String(Float(num1)!*Float(num2)!)
+        }
+        else if operation=="÷"{
+            label1.text = String(Float(num1)!/Float(num2)!)
+            num1=String(Float(num1)!/Float(num2)!)
+        }
+//        else{
+//            label1.text = String(Int(num1)! % Int(num2)!)
+//            num1=String(Int(num1)! % Int(num2)!)
+//        }
+//
+        flip=0
+        c=num1
+        num1="0"
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        num1="0"
+        num2="0"
+        flip=0
+        c=""
+        label1.text=""
+    }
+    
 
 }
 
